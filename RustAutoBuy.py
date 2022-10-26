37b69291887618d9494a2ec6a6c68d4fc30f086c
TheSqueeker <43790023+TheSqueeker@users.noreply.github.com>
2022-10-26 02:56:44 -0500

    Clarity and Information
    changed some variable names to make it better to read and commented out the print for cycle_count to make the terminal less messy and confusing.

--- RustAutoBuy.py
+++ RustAutoBuy.py
@@ -15,45 +15,54 @@ import keyboard, mouse
 hold = True
 vending_time= 0
 
-timeset= input('Going to Outpost?: ')
+print(
+    '|Welcome to RustAutoBuy\n'
+    '|======================\n'
+    '|Go to a vending mechine and open it\n'
+    '|Follow all promts after this\n'
+    '|This script will Auto Buy the max amount of the selected item\n\n'
+    '|To STOP the script press and hold "f" on your keyboard\n'
+)
+
+timeset= input('Going to Outpost? (yes/no): ')
 if timeset == 'Yes' or timeset == 'yes' or timeset == 'YES':
     vending_time= 2.7
 else:
     vending_time= 1.43
-#BB means Buy Button then the Cordanent Plane
+#buy_button means Buy Button then the Cordanent Plane
 #AMT means amount then the Cordanent Plane
 print('Please hover over the Buy button.\nYou have 5 seconds until mouse capture.')
 sleep(5)
-bb= mouse.get_position()
+buy_button= mouse.get_position()
 print('Captured\n')
 sleep(0.3)
 print('Now hover over the numbers below!\n You have 2 seconds until capture.')
 sleep(2)
-atm= mouse.get_position()
+ammount= mouse.get_position()
 print('Captured')
 print('Ok!\nStarting in 5 seconds')
 sleep(5)
 
-bbx= bb[0]
-bby= bb[1]
-amtx= atm[0]
-amty= atm[1]
+buy_button_x= buy_button[0]
+buy_button_y= buy_button[1]
+ammount_x= ammount[0]
+ammount_y= ammount[1]
 
 cycle_count= 0
 #2.8 Outpost
 #1.43 Bandet Camp
 cycle_time= vending_time + 0.66
 while hold == True and keyboard.is_pressed('f') == False:
-    print('Count:',cycle_count,'\nSeconds:',round(cycle_count* cycle_time, 2))
+    #print('Count:',cycle_count,'\nSeconds:',round(cycle_count* cycle_time, 2))
     #if count == 5:
     #    exit()
-    mouse.move(amtx,amty,duration=0.1)
+    mouse.move(ammount_x,ammount_y,duration=0.1)
     sleep(0.2)
     mouse.click(button='left')
     sleep(0.235)
     keyboard.write('99')
     sleep(0.2311)
-    mouse.move(bbx,bby,duration=0.15)
+    mouse.move(buy_button_x,buy_button_y,duration=0.15)
     mouse.click(button='left')
     sleep(vending_time)
     cycle_count= cycle_count+ 1
